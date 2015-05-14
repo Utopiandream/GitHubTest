8eba22b099dd9d16e4bcd8517da73480553f1a57
Stroer, Douglas <Utopiandream247@yahoo.com>
2015-05-14 17:36:16 -0500

    Extras If needed

---
+++ android-BasicMediaDecoder-master/VideoViewer/app/src/main/java/com/dmst3b/projects/videoviewer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.dmst3b.projects.videoviewer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
+        setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.MyButton);
+
+        // Capture button clicks
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        VideoViewActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+}
---
+++ android-BasicMediaDecoder-master/VideoViewer/app/src/main/java/com/dmst3b/projects/videoviewer/VideoViewActivity.java
@@ -0,0 +1,75 @@
+package com.dmst3b.projects.videoviewer;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoViewActivity extends Activity {
+
+    // Declare variables
+    ProgressDialog pDialog;
+    VideoView videoview;
+
+    // Insert your Video URL
+    String VideoURL = "https://youtu.be/9E1hCYVTvZ4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
+        setContentView(R.layout.videoview_main);
+        // Find your VideoView in your video_main.xml layout
+        videoview = (VideoView) findViewById(R.id.VideoView);
+        // Execute StreamVideo AsyncTask
+
+        // Create a progressbar
+        pDialog = new ProgressDialog(VideoViewActivity.this);
+        // Set progressbar title
+        pDialog.setTitle("Android Video Streaming Tutorial");
+        // Set progressbar message
+        pDialog.setMessage("Buffering...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        // Show progressbar
+        pDialog.show();
+
+        try {
+            // Start the MediaController
+            MediaController mediacontroller = new MediaController(
+                    VideoViewActivity.this);
+            mediacontroller.setAnchorView(videoview);
+            // Get the URL from String VideoURL
+            Uri video = Uri.parse(VideoURL);
+            //Or Get the URI of your own video
+            Uri videoUri = Uri.parse("android.resource://"
+                    + getPackageName() + "/"
+                    + R.raw.intro);
+            videoview.setMediaController(mediacontroller);
+            videoview.setVideoURI(video);
+            // Try to add own video
+
+
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoview.requestFocus();
+        videoview.setOnPreparedListener(new OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                pDialog.dismiss();
+                videoview.start();
+            }
+        });
+
+    }
+
+}
