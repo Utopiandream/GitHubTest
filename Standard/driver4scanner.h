d41e2c28f5de77fbf47ad09ac07eefa30343e593
Stroer, Douglas <Utopiandream247@yahoo.com>
2015-11-10 13:22:03 -0600

    Revert "Changes"
    This reverts commit 58333e9bd4b23aeddf5f7bfa9a7a72081f1a6d4d.

--- Standard/driver4scanner.h
+++
@@ -1,7 +0,0 @@
-//driver4scanner.h
-#ifndef DRIVER4SCANNER_H
-#define DRIVER4SCANNER_H
-#include "token.h"
-
-void DRIVER4SCANNER();
-#endif
--- Standard/scanner.h
+++
@@ -1,7 +0,0 @@
-//scanner.h
-#ifndef SCANNER_H
-#define SCANNER_H
-#include "token.h"
-
-void scanner(ifstream &stream, token & tkn);
-#endif
